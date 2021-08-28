d97e31e973079baad9692ee1fe3391eb7adffdb2
Bawa Abba <abbabawa6@gmail.com>
2021-08-28 11:21:49 +0100

    Production build module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
-import './index.css'
-import numeral from "numeral";
 
-const courseValue = numeral(1000).format('$0,0.00')
-console.log(`I would pay ${courseValue} for this awesome course`)//eslint-disable-line no-console
+import { deleteUser, getUsers } from './api/userApi';
+
+getUsers().then(result=>{console.log(result) // eslint-disable-line no-console
+  let userBody = ''
+
+  result.forEach(user=>{
+    userBody +=  `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser" >Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+      <tr>`
+  })
+  global.document.getElementById('users').innerHTML = userBody
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser')
+  Array.from(deleteLinks, link=>{
+    link.onclick = function(event){console.log(event) //eslint-disable-line no-console
+      event.preventDefault()
+      const element = event.target
+      
+      deleteUser(element.attributes['data-id'].value)
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row)
+    }
+  })
+})
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,24 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export default {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.js",
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    main: path.resolve(__dirname, "src/index"),
+    vendor: path.resolve(__dirname, "src/vendor")
+  },
   output: {
-    path: path.resolve(__dirname, "src"),
+    path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "bundle.js",
+    filename: "[name].[chunkhash].js",
   },
   plugins: [
+    //Generate an external css file with a hash in the filename
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
     //Create HTML file that includes reference to bundle
     new HtmlWebpackPlugin({
       template: "src/index.html"
